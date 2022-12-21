fc8b0fd522e67bc740a3c6d7194f0a53758f77ac
elie haddad <haddadelie12@gmail.com>
2022-12-21 22:51:31 +0200

    d

--- Web-Servers.sh
+++ Web-Servers.sh
@@ -2,4 +2,4 @@
 sudo apt update -y
 sudo apt install apache2 -y
 sudo systemctl start apache2
-sudo echo "Rasheed's Web Server " > /var/www/html/index.html
+sudo echo "hw6 elie Web Server " > /var/www/html/index.html
